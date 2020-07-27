ecafdf2aca1c0ddcaca2cb65f0b0a77b6a5870fd
Jon Insley <jinsley8@gmail.com>
2020-07-26 23:45:45 -0400

    use onPress to set background color using state

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
 
+function ColorButton({ backgroundColor, onPress = (f) => f }) {
+  return (
+    <TouchableHighlight
+      style={styles.button}
+      onPress={() => onPress(backgroundColor)}
+      underlyingColor="orange"
+    >
+      <View style={styles.row}>
+        <View style={[styles.sample, { backgroundColor }]} />
+        <Text style={styles.buttonText}>{backgroundColor}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+}
+
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
+
   return (
     <View style={[styles.container, { backgroundColor }]}>
-      <TouchableHighlight
-        style={styles.button}
-        onPress={() => setBackgroundColor("yellow")}
-        underlyingColor="orange"
-      >
-        <View style={styles.row}>
-          <View style={[styles.sample, { backgroundColor: "yellow" }]} />
-          <Text style={styles.buttonText}>Yellow</Text>
-        </View>
-      </TouchableHighlight>
+      <ColorButton backgroundColor="red" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="green" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="blue" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="yellow" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="purple" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="pink" onPress={setBackgroundColor} />
     </View>
   );
 }
