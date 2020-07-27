ab481981c2db3c040460d25f2efd4d242f03b854
Jon Insley <jinsley8@gmail.com>
2020-07-26 23:55:36 -0400

    implement FlatList component to render list

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import ColorButton from "./components/ColorButton";
+import defaultColors from "./data/defaultColors.json";
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
 
   return (
-    <View style={[styles.container, { backgroundColor }]}>
-      <ColorButton backgroundColor="red" onPress={setBackgroundColor} />
-      <ColorButton backgroundColor="green" onPress={setBackgroundColor} />
-      <ColorButton backgroundColor="blue" onPress={setBackgroundColor} />
-      <ColorButton backgroundColor="yellow" onPress={setBackgroundColor} />
-      <ColorButton backgroundColor="purple" onPress={setBackgroundColor} />
-      <ColorButton backgroundColor="pink" onPress={setBackgroundColor} />
-    </View>
+    <FlatList
+      style={[styles.container, { backgroundColor }]}
+      data={defaultColors}
+      renderItem={({ item }) => {
+        return (
+          <ColorButton
+            key={item.id}
+            backgroundColor={item.color}
+            onPress={setBackgroundColor}
+          />
+        );
+      }}
+    />
   );
 }
 
@@ -21,7 +27,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
