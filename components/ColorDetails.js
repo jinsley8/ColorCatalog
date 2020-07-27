e8663b8d23d77cb2847b5cf264eef0a92c2f79da
Jon Insley <jinsley8@gmail.com>
2020-07-27 01:33:10 -0400

    get color details for each color page

--- components/ColorDetails.js
+++ components/ColorDetails.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Color from "color";
 
 export default function ColorDetails({ route }) {
+  const { color: name } = route.params;
+  const color = Color(name);
+  const textColor = { fontSize: 30, color: color.negate().toString() };
+
   return (
-    <View style={styles.container}>
-      <Text>Color Details: {route.params.color}</Text>
+    <View style={[styles.container, { backgroundColor: color }]}>
+      <Text style={textColor}>{name}</Text>
+      <Text style={textColor}>{color.rgb().toString()}</Text>
+      <Text style={textColor}>{color.hsl().toString()}</Text>
+      <Text style={textColor}>{color.luminosity()}</Text>
     </View>
   );
 }
