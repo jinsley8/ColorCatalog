f30e67284d3dc89c036b86c5ab3cf78429259393
Jon Insley <jinsley8@gmail.com>
2020-07-26 22:54:59 -0400

    added stylesheet to test adding styles to text

--- App.js
+++ App.js
@@ -1,38 +1,30 @@
 import React from "react";
-import {
-  Alert,
-  Button,
-  View,
-  ActivityIndicator,
-  ProgressViewIOS,
-  ProgressBarAndroid,
-  Dimensions,
-  Text,
-  Platform,
-} from "react-native";
-
-const { height, width } = Dimensions.get("window");
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const onButtonPress = () => {
-    Alert.alert(`${new Date().toLocaleTimeString()} button press`);
-  };
   return (
-    <View style={{ padding: 50 }}>
-      {Platform.OS === "ios" && <ProgressViewIOS progress={0.5} />}
-      {Platform.OS === "android" && (
-        <ProgressBarAndroid
-          styleAttr="Horizontal"
-          indeterminate={false}
-          color="blue"
-          progress={0.3}
-        />
-      )}
-      <ActivityIndicator size="large" color="#61DBFB" />
-      <Button title="Click Me" onPress={onButtonPress} />
-      <Text>OS: {Platform.OS}</Text>
-      <Text>Height: {height}</Text>
-      <Text>Width: {width}</Text>
+    <View style={styles.page}>
+      <Text style={styles.text}>red</Text>
+      <Text style={[styles.text, styles.selectedText]}>green</Text>
+      <Text style={styles.text}>blue</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    marginTop: 40,
+    backgroundColor: "#DDD",
+  },
+  text: {
+    fontSize: 22,
+    color: "red",
+    backgroundColor: "yellow",
+    margin: 10,
+    padding: 5,
+  },
+  selectedText: {
+    color: "yellow",
+    backgroundColor: "red",
+  },
+});
