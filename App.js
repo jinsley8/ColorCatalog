d7e960d1f115f860f1a570cfa02e8475cf904161
Jon Insley <jinsley8@gmail.com>
2020-07-26 23:27:29 -0400

    implement local state changes with useState hook

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
-import React from "react";
-import { StyleSheet, Image, View, Dimensions } from "react-native";
-
-import picBiscuit from "./assets/biscuit.jpg";
-import picJungle from "./assets/jungle.jpg";
+import React, { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
-    <View style={styles.page}>
-      <Image style={styles.image} source={picBiscuit} />
-      <Image style={styles.image} source={picJungle} />
+    <View style={[styles.container, { backgroundColor }]}>
+      <Text style={styles.button} onPress={() => setBackgroundColor("green")}>
+        green
+      </Text>
+      <Text style={styles.button} onPress={() => setBackgroundColor("red")}>
+        red
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  page: {
+  container: {
     flex: 1,
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
   },
-  image: {
-    flex: 1,
-    borderRadius: 50,
+  button: {
+    fontSize: 30,
     margin: 10,
-    width: Dimensions.get("window").width - 10,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    alignSelf: "stretch",
+    textAlign: "center",
   },
 });
